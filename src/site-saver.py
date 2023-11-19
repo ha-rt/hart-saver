828466f1696fd7ec24f7c8a809ce4f3a56faf862
Oliver <oliverm.development@proton.me>
2023-11-19 15:37:31 -0800

    added multiple repeating

--- src/site-saver.py
+++ src/site-saver.py
@@ -3,21 +3,40 @@ from parser import returnLinks
 saved_links = [
 
 ]
+repeating = False
+repeated,repeated_max = 0,0
+
 
 def refineFile(file):
     try:
-        with open(f'{file}.txt', 'r') as file:
-            lines = file.readlines()
-            unique_lines = set(lines)
-        with open(f'{file}.txt', 'w') as file:
-            file.writelines(unique_lines)
+        with open(f'{file}.txt', 'r') as input_file:
+            lines = input_file.readlines()
+        unique_lines = set(lines)
+        with open(f'{file}.txt', 'w') as output_file:
+            output_file.writelines(unique_lines)
     except FileNotFoundError:
         print(f"File '{file}.txt' not found.")
 
 def continueMenu(links, site, file):
     saveLinks(links, file)
+
+    if repeating == True and repeated != repeated_max:
+        new_links = []
+        for link in links:
+            if link not in saved_links:
+                saved_links.append(link)
+                nlks = returnLinks(link)
+                saveLinks(nlks, file)
+                new_links.extend(nlks)
+        continueMenu(new_links, site, file)
+        return
     print(f"The links for {site} have been saved to {file}.txt")
     cont = input("Would you like to continue parsing? (y/n)")
+
+    if cont == "n":
+        refineFile(file)
+
+    cont_repeat = input("How many times would you like to parse? (num)")
 
     if cont == "y":
         new_links = []
@@ -28,9 +47,6 @@ def continueMenu(links, site, file):
                 saveLinks(nlks, file)
                 new_links.extend(nlks)
         continueMenu(new_links, site, file)
-
-    if cont == "n":
-        refineFile()
 
 def saveLinks(list, file):
     for link in list:
